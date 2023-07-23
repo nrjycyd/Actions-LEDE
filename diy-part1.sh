ec6f1fdcbc2c8e570dc76ebedd86559984973d18
nrjycyd <77444933+nrjycyd@users.noreply.github.com>
2023-07-23 22:55:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,8 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i 's/#src-git helloworld/src-git helloworld/g' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git ssrp https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git ssrp https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
