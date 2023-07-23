7e6315121401314c0e716f09e2806988c30349e8
nrjycyd <77444933+nrjycyd@users.noreply.github.com>
2023-07-23 22:09:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,8 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 
 # Add a feed source
 #echo 'src-git ssrp https://github.com/fw876/helloworld' >>feeds.conf.default
