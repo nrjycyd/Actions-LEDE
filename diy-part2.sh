60f41ade1eb08f1f0d3db35855781be34d64fe5e
nrjycyd <77444933+nrjycyd@users.noreply.github.com>
2023-07-24 11:20:27 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
+
+echo 'src-git ssrp https://github.com/fw876/helloworld' >>feeds.conf.default
