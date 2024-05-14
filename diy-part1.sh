46f142d2d61bb515ac53ed69edeafb3a348253b5
nrjycyd <77444933+nrjycyd@users.noreply.github.com>
2024-05-14 23:28:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,10 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+#Switch 23.05 luci
+sed -i 's/^\(.*luci\)/#\1/' feeds.conf.default
+sed -i 's/^#\(src-git luci https:\/\/github\.com\/coolsnowwolf\/luci\.git;openwrt-23\.05\)/\1/' feeds.conf.default
+
 # Add a feed source
 #echo "src-git ssrplus https://github.com/fw876/helloworld.git;main" >> "feeds.conf.default"
 #echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
