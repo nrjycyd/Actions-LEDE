070440ebd6f703fd1353bc6dbe8bc9a770a0f7f8
nrjycyd <77444933+nrjycyd@users.noreply.github.com>
2024-10-22 11:38:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,8 +11,8 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
-# sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/luci2/bin/config_generate # 23.05
+sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate # 18.06 LuCI
+# sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/luci2/bin/config_generate # 23.05 LuCI
 
 # Modify default KERNEL_PATCHVER
 # sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' target/linux/x86/Makefile
@@ -22,5 +22,5 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='LEDE'' package/
 
 # Add luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
-# git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon # 23.05
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon # 18.06 LuCI
+# git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon # 23.05 LuCI
